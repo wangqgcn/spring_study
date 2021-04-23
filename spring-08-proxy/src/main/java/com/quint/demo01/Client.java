495e5dd4d984f54eb1d5b7fc7a0b014f929ce820
wangqgcn <w565422579@gamil.com>
2021-04-23 16:33:43 +0800

    1

---
+++ spring-08-proxy/src/main/java/com/quint/demo01/Client.java
@@ -0,0 +1,8 @@
+package com.quint.demo01;
+
+public class Client {
+    public static void main(String[] args) {
+        Host host = new Host();
+        host.rent();
+    }
+}
---
+++ spring-08-proxy/src/main/java/com/quint/demo01/Rent.java
@@ -0,0 +1,5 @@
+package com.quint.demo01;
+
+public interface Rent {
+    public void rent();
+}
